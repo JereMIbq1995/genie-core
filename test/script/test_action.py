15365dd38f6bf9b487d190cdb812014028ccb54b
jeremi1995 <jeremibq@gmail.com>
2021-09-20 15:37:47 -0600

    Changed priority from type string to type int

--- test/script/test_action.py
+++ test/script/test_action.py
@@ -7,6 +7,10 @@ sys.path.append('..')
 
 from genie.script.action import Action
 
+PRIORITY_ONE = 1
+PRIORITY_TWO = 2
+PRIORITY_THREE = 3
+
 class TestAction(unittest.TestCase):
     
     class MockAction(Action):
@@ -18,9 +22,9 @@ class TestAction(unittest.TestCase):
         pass
 
     def setUp(self):
-        self._action1 = self.MockAction("PRIORITY_ONE")
-        self._action2 = self.MockAction("PRIORITY_TWO")
-        self._action3 = self.MockAction("PRIORITY_THREE")
+        self._action1 = self.MockAction(PRIORITY_ONE)
+        self._action2 = self.MockAction(PRIORITY_TWO)
+        self._action3 = self.MockAction(PRIORITY_THREE)
 
     def tearDown(self):
         pass
@@ -30,26 +34,26 @@ class TestAction(unittest.TestCase):
         Ensures that get_priority returns the same priority
         passed to it originally
         """
-        self.assertEqual(self._action1.get_priority(), "PRIORITY_ONE")
-        self.assertEqual(self._action2.get_priority(), "PRIORITY_TWO")
-        self.assertEqual(self._action3.get_priority(), "PRIORITY_THREE")
+        self.assertEqual(self._action1.get_priority(), PRIORITY_ONE)
+        self.assertEqual(self._action2.get_priority(), PRIORITY_TWO)
+        self.assertEqual(self._action3.get_priority(), PRIORITY_THREE)
     
     def test_set_priority_correctly_changes_the_priority(self):
         """
         Ensures that the priority of the action whose "set_priority" function
         is called is changed to the appropriate priority
         """
-        # Changing action1 to "PRIORITY_TWO":
-        self._action1.set_priority("PRIORITY_TWO")
-        self.assertEqual(self._action1._priority, "PRIORITY_TWO")
+        # Changing action1 to PRIORITY_TWO:
+        self._action1.set_priority(PRIORITY_TWO)
+        self.assertEqual(self._action1._priority, PRIORITY_TWO)
 
-        # Changing action2 to "PRIORITY_THREE":
-        self._action2.set_priority("PRIORITY_THREE")
-        self.assertEqual(self._action2._priority, "PRIORITY_THREE")
+        # Changing action2 to PRIORITY_THREE:
+        self._action2.set_priority(PRIORITY_THREE)
+        self.assertEqual(self._action2._priority, PRIORITY_THREE)
 
-        # Changing action3 to "PRIORITY_ONE":
-        self._action3.set_priority("PRIORITY_ONE")
-        self.assertEqual(self._action3._priority, "PRIORITY_ONE")
+        # Changing action3 to PRIORITY_ONE:
+        self._action3.set_priority(PRIORITY_ONE)
+        self.assertEqual(self._action3._priority, PRIORITY_ONE)
 
 if __name__ == "__main__":
     unittest.main()
