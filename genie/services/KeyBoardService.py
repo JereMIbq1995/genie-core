54c12629d7d1d583c451a93014661914320ff7c1
jeremi1995 <jeremibq@gmail.com>
2021-09-22 17:31:37 -0600

    Initial skeletons for services

---
+++ genie/services/KeyBoardService.py
@@ -0,0 +1,10 @@
+
+class KeyBoardService():
+    def __init__(self):
+        pass
+
+    def on_key_press(self, key):
+        pass
+
+    def on_key_release(self, key):
+        pass
---
+++ genie/services/MouseService.py
@@ -0,0 +1,13 @@
+
+class MouseService:
+    def __init__(self):
+        pass
+
+    def on_mouse_click(self, x, y, dx, dy):
+        pass
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        pass
+
+    def on_mouse_release(self, x, y, dx, dy):
+        pass
